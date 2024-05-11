1f5c23b1f82b4a1a2d71da34288317b508abe59d
Dinesh <dinesh.bali@bobble.ai>
2024-05-11 18:35:59 -0500

    updated code

--- database/migrations/0001_create_music_table.up.sql
+++ database/migrations/0001_create_music_table.up.sql
@@ -1,14 +1,13 @@
-CREATE TABLE music (
+CREATE TABLE songs(
     `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'Reepresents unique id for the song',
-    `song_name` varchar(30) NOT NULL COMMENT 'Represents name of the song',
-    `song_file` MEDIUMBLOB NOT NULL COMMENT 'Represents file that contians binary data related to the song',
+    `name` varchar(30) NOT NULL COMMENT 'Represents name of the song',
+    `file` MEDIUMBLOB NOT NULL COMMENT 'Represents file that contians binary data related to the song',
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Represents timestamp at which the record is created ',
     PRIMARY KEY(id)
 ) Engine = InnoDB
 DEFAULT CHARSET = utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT = 'Table to store the music related data';
 
-ALTER TABLE music
-    ADD UNIQUE KEY `song_name_idx`(song_name);
-
+ALTER TABLE songs
+    ADD UNIQUE KEY `song_name_idx`(name);
 -- BLOB (binary large object) we use this type of datatyp to store videos, music etc..
 -- we cannot add index to BLOB type memory storage and also BLOB
