ae6ebfb3d34399faeb2e6210913fcbefa2004f7e
Bali <S572106@nwmissouri.edu>
2024-05-17 10:33:50 -0500

    updated migrations

--- database/migrations/0002_alter_music_table.up.sql
+++ database/migrations/0002_alter_music_table.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE Musics ADD COLUMN link VARCHAR(30) NULL COMMENT 'Represents song link' AFTER file;
-ALTER TABLE Musics ADD COLUMN image VARCHAR(30) NULL COMMENT 'Represents song image link' AFTER file;
+ALTER TABLE Musics ADD COLUMN link VARCHAR(500) NULL COMMENT 'Represents song link' AFTER file;
+ALTER TABLE Musics ADD COLUMN image VARCHAR(500) NULL COMMENT 'Represents song image link' AFTER file;
